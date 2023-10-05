a6cc18daf78e2f58528de4032c87c91c6e468eac
leviLThomas <levi.thomas5384@gmail.com>
2023-10-04 18:44:01 -0600

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT301F23T31"
+include(":app")
+ 
